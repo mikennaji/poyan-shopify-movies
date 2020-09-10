300948abdfd8e83b115405108ca338272a321fa7
Po Yan Ng <poyanng.npy@gmail.com>
2020-09-10 16:05:25 -0400

    Able to call API and deleted Movie model

--- app/models/movie.rb
+++
@@ -1,4 +0,0 @@
-class Movie < ApplicationRecord
-  has_many :nominations, dependent: :destroy
-  has_many :users, through: :nominations
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :movies, only: [:index]
+  resources :nominations, only: [:index, :new, :create, :destroy]
 end
