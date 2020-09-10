3349058e54986e8149d4c64059021aebcca2e23a
Po Yan Ng <poyanng.npy@gmail.com>
2020-09-10 16:37:13 -0400

    able to add nominations

--- db/migrate/20200910201404_create_nominations.rb
+++ db/migrate/20200910201404_create_nominations.rb
@@ -2,7 +2,7 @@ class CreateNominations < ActiveRecord::Migration[6.0]
   def change
     create_table :nominations do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :movie, null: false, foreign_key: true
+      t.string :imdb_id
 
       t.timestamps
     end
